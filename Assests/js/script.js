a2d8e3eae6bda6bf1f379bf5348b68783f5e9c07
chintanvaja <chintanvaja007@gmail.com>
2024-03-07 16:45:10 +0530

    slider update

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -1,3 +1,5 @@
+/* Toggle JS*/
+
 var nav = document.getElementById('navlist');
 var tog = document.getElementById('toggle-nav');
 	
@@ -22,11 +24,34 @@ var tog = document.getElementById('toggle-nav');
             }
 		}
 
-	
-	
 		function windowResizeHandler () {
 			if ( screen.width > 768 )
 			nav.style.display = "";
 		}
 	
 		window.addEventListener("resize", windowResizeHandler);
+
+/* SlideShow JS */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentDiv(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("event_slide");
+  let dots = document.getElementsByClassName("event_btn1");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
+}
